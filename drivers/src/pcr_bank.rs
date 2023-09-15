b6117733075f1d1e5c9f4a1c0dd9e3dc6d04c62d
Leon Gross <leon.gross@9elements.com>
2023-09-15 13:37:19 +0200

    add documentation for function 'pcr_extend', align spelling of 'PCR'

--- drivers/src/pcr_bank.rs
+++ drivers/src/pcr_bank.rs
@@ -117,9 +117,9 @@ impl PcrBank {
     pub fn new(pv: PvReg) -> Self {
         Self { pv, log_index: 0 }
     }
-    /// Erase all the pcrs in the pcr vault
+    /// Erase all the PCRs in the PCR vault
     ///
-    /// Note: The pcrs that have "use" lock set will not be erased
+    /// Note: The PCRs that have "use" lock set will not be erased
     pub fn erase_all_pcrs(&mut self) {
         const PCR_IDS: [PcrId; 32] = [
             PcrId::PcrId0,
@@ -164,7 +164,7 @@ impl PcrBank {
         }
     }
 
-    /// Erase specified pcr
+    /// Erase specified PCR
     ///
     /// # Arguments
     ///
